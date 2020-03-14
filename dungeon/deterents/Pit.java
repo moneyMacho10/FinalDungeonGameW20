bea56d7d9b29889b53065e910b66a0c4b7f2ffce
JoshStewart98 <56612667+miasmith5@users.noreply.github.com>
2020-03-14 14:41:49 -0700

    Completed Pillar.java and finishing up Pit.java

--- dungeon/deterents/Pit.java
+++ dungeon/deterents/Pit.java
@@ -1,13 +1,19 @@
 package dungeon.deterents;
+import java.util.Random;
 
 public class Pit {
-	private int damage;
+	private int minDamage;
+	private int maxDamage;
+	private int damageDealt;
 	
 	Pit(){
-		
+		this.minDamage = 10;
+		this.maxDamage = 30;
 	}
 	
-	public void fallIn() {
+	public void fallInPit() {
+		Random randomDamage = new Random();
+		
 		
 	}
 }
--- dungeon/pillars/Pillar.java
+++ dungeon/pillars/Pillar.java
@@ -1,18 +1,75 @@
 package dungeon.pillars;
+import java.util.Random;
 
 public class Pillar {
-	private boolean found;
+	private int numberOfColumns;
+	private String encapsulation;
+	private String abstraction;
+	private String inheritance;
+	private String polymorphism;
 	
-	Pillar(){
-		this.found = false;
+	public Pillar() {
+		this.encapsulation = null;
+		this.abstraction = null;
+		this.inheritance = null;
+		this.polymorphism = null;
+		this.numberOfColumns = 0;
 	}
 	
-	public boolean isFound() {
-		return this.found;
+	public String getPillar() {
+		Random randomNumber = new Random();
+		int randomPillar = randomNumber.nextInt(4);
+		
+		if(randomPillar == 0 && this.encapsulation == null) {
+			this.encapsulation = "Encapsulation";
+			this.numberOfColumns++;
+			return this.encapsulation;
+		
+		}else if(randomPillar == 1 && this.abstraction == null) {
+			this.abstraction = "Abstraction";
+			this.numberOfColumns++;
+			return this.abstraction;
+			
+		}else if(randomPillar == 2 && this.inheritance == null) {
+			this.inheritance = "Inheritance";
+			this.numberOfColumns++;
+			return this.inheritance;
+			
+		}else if(randomPillar == 3 && this.polymorphism == null) {
+			this.polymorphism = "Polymorphism";
+			this.numberOfColumns++;
+			return this.polymorphism;
+			
+		}else {
+			return "Error: Random number generator for pillars is incorrect"
+		}
 	}
 	
-	public void setFound(boolean f) {
-		this.found = f;
+	public boolean haveAllColumns() {
+		if(this.numberOfColumns == 4)
+			return true;
+		
+		else
+			return false;
+	}
+	
+	
+	public String getFoundColumns() {
+		String result = "";
+		
+		if(this.encapsulation != null)
+			result += this.encapsulation + " ";
+		
+		if(this.abstraction != null)
+			result += this.abstraction + " ";
+		
+		if(this.inheritance != null)
+			result += this.inheritance + " ";
+		
+		if(this.polymorphism != null)
+			result += this.polymorphism;
+		
+		return result;
 	}
 	
 	
